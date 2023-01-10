41d2cad54e3cbe8e2688e2854d40698c007e7137
Алексей Попов <alexseipopov@MacBook-Pro-Alexsei.local>
2023-01-10 19:13:16 +0300

    add 2 files PNG and JS

---
+++ script.js
@@ -0,0 +1,15 @@
+const first = document.querySelector("#first")
+const second = document.getElementById("second")
+const third = document.getElementById("third")
+
+first.addEventListener("click", () => console.log(1), true)
+second.addEventListener("click", () => {
+    console.log(2)
+})
+third.addEventListener("click", (e) => {
+    e.stopPropagation()
+    console.log(3)
+})
+
+let a = 10
+a = 5
